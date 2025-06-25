ffb8b7f330098c793112ef55b555557b7a5bccfa
cblancac <carblacac7@gmail.com>
2025-06-25 20:09:12 +0200

    add tokenization 1536 for inference

--- scripts/inference.py
+++ scripts/inference.py
@@ -295,7 +295,7 @@ def extract_cv(cv_text):
             {"role": "system", "content": SYSTEM_PROMPT},
             {"role": "user", "content": cv_text}
         ],
-        max_tokens=1024, #Dejar en 512
+        max_tokens=888, #Dejar en 512
         temperature=0.0,
     )
     
